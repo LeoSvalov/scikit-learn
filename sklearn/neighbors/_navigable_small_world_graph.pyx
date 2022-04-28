798bc869d5655ff2adb90787af83161626439591
LeoSvalov <leos3112@gmail.com>
2022-04-29 00:55:47 +0300

    well i'm stupid

--- sklearn/neighbors/_navigable_small_world_graph.pyx
+++ sklearn/neighbors/_navigable_small_world_graph.pyx
@@ -27,10 +27,10 @@ cdef class NSWGraph:
     #     self.regularity = self.dimension // 2 if reg==0 else reg
     #     self.guard_hops = guard_hops
 
-    def __init__(self, ITYPE_t reg, ITYPE_t n_neighbors=5, ITYPE_t guard_hops=100):
+    def __init__(self, ITYPE_t reg):#, ITYPE_t n_neighbors=5, ITYPE_t guard_hops=100):
         self.regularity = reg
-        self.n_neigbours = n_neighbors
-        self.guard_hops = guard_hops
+        self.n_neigbours = 5 #n_neighbors
+        self.guard_hops = 100 #guard_hops
 
 
 
