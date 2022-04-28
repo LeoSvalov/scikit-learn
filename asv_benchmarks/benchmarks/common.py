b471c15b63633c535aba2c64f7f3f32274f13879
LeoSvalov <leos3112@gmail.com>
2022-04-28 20:03:25 +0300

    debug setup-cache

--- asv_benchmarks/benchmarks/common.py
+++ asv_benchmarks/benchmarks/common.py
@@ -144,15 +144,15 @@ class Estimator(ABC):
         param_grid = list(itertools.product(*self.params))
 
         #
-        # for params in param_grid:
-        #     if self.skip(params):
-        #         continue
-        #
-        #     estimator = self.make_estimator(params)
+        for params in param_grid:
+            if self.skip(params):
+                continue
+
+            # estimator = self.make_estimator(params)
             # X, _, y, _ = self.make_data(params)
             #
             # estimator.fit(X, y)
-
+            #
             # est_path = get_estimator_path(
             #     self, Benchmark.save_dir, params, Benchmark.save_estimators
             # )
